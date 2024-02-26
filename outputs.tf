5f879d500a5afec2dba3257d80583ed18ed43ed3
Rhubarb Sin <rhubarbsin@brianshrub.xyz>
2024-02-25 23:01:21 -0500

    specify region for session

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,12 @@ output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
 }
 
+output "instance_region" {
+  value = var.region
+
+  description = "The region in which the EC2 instance resides"
+}
+
 output "ssh_key_file_name" {
   value = aws_key_pair.this.key_name
 
